4eea71804e01472d5467a594c1bd2104c33ae783
chelsea-she <11chelseashe@gmail.com>
2024-11-23 22:01:42 -0500

    Initial Commit

---
+++ Found/FoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoundApp.swift
+//  Found
+//
+//  Created by Chelsea She on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct FoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
